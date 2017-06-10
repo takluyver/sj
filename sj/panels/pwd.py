be4126f47b1cb207adfa4936c5573b804b8e6e3b
Thomas Kluyver <thomas@kluyver.me.uk>
2017-06-10 20:13:20 +0100

    Remove unused code

--- sj/panels/pwd.py
+++ sj/panels/pwd.py
@@ -1,18 +1,10 @@
 from gi.repository import Gtk, GLib
 from sj.utils import compress_user
 
-css = b"""
-GtkLabel {
-    font-size: x-large;
-}
-"""
-
 class PathLabel(Gtk.Label):
     larger = False
     def __init__(self, window):
         super().__init__()
-        self.style_provider = Gtk.CssProvider()
-        self.style_provider.load_from_data(css)
         self.set_markup("Configure your shell to run <b>$SJ_UPDATE_COMMAND</b> "
                         "at each prompt.")
         window.connect('wd_changed', self.wd_changed)
