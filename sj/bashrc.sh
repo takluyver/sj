13bde1019d28bfceebf2eb2f1fe735ff639cf972
Thomas Kluyver <thomas@kluyver.me.uk>
2023-06-30 17:28:30 +0100

    Add to existing PROMPT_COMMAND if it is set

--- sj/bashrc.sh
+++ sj/bashrc.sh
@@ -1,4 +1,8 @@
 # Run the regular bashrc file
 source ~/.bashrc
 
-export PROMPT_COMMAND=$SJ_UPDATE_COMMAND
+if [[ -z "$PROMPT_COMMAND" ]]; then
+  export PROMPT_COMMAND="$SJ_UPDATE_COMMAND"
+else
+  export PROMPT_COMMAND="$PROMPT_COMMAND;$SJ_UPDATE_COMMAND"
+fi
